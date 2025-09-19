5010ab5579dce2e35e9355e17800d8703e4c00f8
mek0124 <mek0124@proton.me>
2025-09-19 00:54:07 -0500

    redone entry layout; added two buttons; created delete function;

--- client/src/pages/history.js
+++ client/src/pages/history.js
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPencil, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 import api from "../hooks/api";
 
 
 export default function History() {
-  const [previousEntries, setPreviousEntries] = useState();
+  const [previousEntries, setPreviousEntries] = useState(null);
 
   useEffect(() => {
     async function fetchEntries() {
@@ -16,17 +18,71 @@ export default function History() {
         };
       } catch (err) {
         console.error(err);
-        setPreviousEntries([]);
+        setPreviousEntries(null);
       };
     };
 
     fetchEntries();
   }, []);
 
+  const deleteEntry = async (entryId) => {
+    try {
+      await api.delete(`/diary/${entryId}`);
+      window.location.reload();
+    } catch (err) {
+      console.error(err);
+    };
+  };
+
   const displayEntries = () => {
     return previousEntries.map((entry) => {
+      const entryDate = entry.createdAt;
+      const date = entryDate.split("T")[0];
+      const time = entryDate.split("T")[1];
+
       return (
-        <div className="flex flex-row items-center justify-center w-[30%] border-2 border-accent rounded"></div>
+        <div
+          key={entry._id} 
+          className="flex flex-col items-center justify-center w-[25%] border-2 border-colorAccent rounded-xl min-h-72">
+          
+          <div className="flex flex-row items-start justify-center w-full ml-3">
+            <label className="font-bold italic text-2xl text-center w-full text-colorTextPrimary max-h-1/3">
+              {entry.title}
+            </label>
+
+            <label className="italic text-xs text-center w-full text-colorTextPrimary mr-0 max-h-1/3">
+              {date}
+            </label>
+
+            <label className="italic text-xs text-center w-full text-colorTextPrimary ml-0 max-h-1/3">
+              {time}
+            </label>
+          </div>
+
+          <div className="flex flex-col items-center justify-center w-full text-wrap text-[#a9a9a9] mr-4 flex-grow">
+            <label className="italic text-sm text-colorTextPrimary">
+              {entry.details}
+            </label>
+          </div>
+
+          <div className="flex flex-row items-center justify-evenly w-1/3">
+            <button
+              type="button"
+              onClick=""
+              className="text-colorTextPrimary text-sm m-2">
+
+              <FontAwesomeIcon icon={faPencil} />
+            </button>
+
+            <button
+              type="button"
+              onClick={() => deleteEntry(entry._id)}
+              className="text-colorTextPrimary text-sm m-2">
+
+              <FontAwesomeIcon icon={faTrash} />
+            </button>
+          </div>
+        </div>
       );
     });
   };
@@ -50,7 +106,7 @@ export default function History() {
       </div>
 
       <div className="flex flex-col items-center justify-start w-full">
-        <div className="flex flex-col items-center justify-start w-[90%] border-2 border-colorAccent rounded-xl overflow-y-auto min-h-[650px]">
+        <div className="flex flex-row flex-wrap items-start justify-center w-[95%] border-2 border-colorAccent rounded-xl overflow-y-auto min-h-[650px] gap-5 p-2">
           { displayEntries() }
         </div>
       </div>
